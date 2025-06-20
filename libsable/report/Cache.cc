76fe43897650c3037995927533a3ce4526c93956
Fabrice <fabrice.schaub@swisscom.com>
2025-06-20 12:53:39 +0200

    improving on caching

--- libsable/report/Cache.cc
+++ libsable/report/Cache.cc
@@ -2,4 +2,23 @@
 
 namespace sable::report {
 Line::Line(common::Range<std::size_t> range) : range(range) {}
+CacheEntry::CacheEntry(std::shared_ptr<common::Source> source)
+    : source(std::move(source)) {
+  if (!this->source) {
+    return;
+  }
+
+  std::size_t start = 0;
+  std::size_t end = 0;
+  std::string_view content = this->source->content;
+  std::size_t length = content.length();
+  while (end < length) {
+    if (content[end] == '\n') {
+      lines.emplace_back(common::Range<std::size_t>(start, end));
+      start = end + 1;
+    }
+    ++end;
+  }
+}
+
 } // namespace sable::report
--- libsable/report/Cache.h
+++ libsable/report/Cache.h
@@ -5,6 +5,7 @@
 #include <cstddef>
 #include <memory>
 #include <unordered_map>
+#include <vector>
 
 namespace sable::report {
 struct Line {
@@ -17,6 +18,9 @@ public:
 struct CacheEntry {
 public:
   std::shared_ptr<common::Source> source;
+  std::vector<Line> lines;
+
+  CacheEntry(std::shared_ptr<common::Source> source);
 };
 
 template <typename S> class Cache {
@@ -34,5 +38,27 @@ template <typename S> class Cache {
 private:
   const common::Manager &manager;
   std::unordered_map<S, CacheEntry> entries;
+
+public:
+  explicit Cache(const common::Manager &manager) : manager(manager) {}
+
+  void addEntry(const S &span) {
+    auto it = entries.find(span);
+    if (it == entries.end()) {
+      std::shared_ptr<common::Source> source =
+          manager.getContent(span.source());
+      if (source) {
+        entries.emplace(span, CacheEntry(source));
+      }
+    }
+  }
+
+  std::optional<const CacheEntry *> getEntry(const S &span) const {
+    auto it = entries.find(span);
+    if (it != entries.end()) {
+      return &it->second;
+    }
+    return std::nullopt;
+  }
 };
 } // namespace sable::report
