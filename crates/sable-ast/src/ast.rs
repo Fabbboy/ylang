7c5b3a6b09309c9eee4ab10eb054917964faa3ab
Fabrice <67000409+Fabbboy@users.noreply.github.com>
2025-07-01 18:27:38 +0200

    feat: add optional serde support

--- crates/sable-ast/src/ast.rs
+++ crates/sable-ast/src/ast.rs
@@ -2,11 +2,13 @@ use getset::{
   Getters,
   MutGetters,
 };
+#[cfg(feature = "serde")]
 use serde::Serialize;
 
 use crate::objects::function::Function;
 
-#[derive(Getters, MutGetters, Debug, Serialize)]
+#[derive(Getters, MutGetters, Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize))]
 pub struct Ast {
   #[getset(get_mut = "pub", get = "pub")]
   funcs: Vec<Function>,
