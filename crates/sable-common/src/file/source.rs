c9270f7040b109a12bb3bf73cde16b0bfea07ad1
Fabrice <fabrice@schaub-dev.xyz>
2025-07-10 13:36:05 +0200

    fixed leak

--- crates/sable-common/src/file/source.rs
+++ crates/sable-common/src/file/source.rs
@@ -18,6 +18,7 @@ impl<'ctx> Source<'ctx> {
     let arced_raw_ptr = Arc::into_raw(arced); // is leaking we need to recreate the original again without copying to destruct it properly
 
     let arc = unsafe { Arc::<str, &'ctx Arena>::from_raw_in(arced_raw_ptr, arena) }; // from_raw expects an Arc not a str but its the only way we can create a arc with custom allocator KEEP IT!!!!
+    let _ = unsafe { Arc::from_raw(arced_raw_ptr) };
 
     Self {
       content: arena.alloc_str(content),
