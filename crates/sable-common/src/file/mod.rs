f90bf56590dcf49ce9690f1e3d259e6206a4d9d1
Fabrice <fabrice@schaub-dev.xyz>
2025-07-10 13:44:08 +0200

    using direct ref

--- crates/sable-common/src/file/mod.rs
+++ crates/sable-common/src/file/mod.rs
@@ -1,35 +1,7 @@
-use std::{
-  fmt::Display,
-  ops::Range,
-  sync::Arc,
-};
-
-use sable_arena::arena::Arena;
+use std::ops::Range;
 
 pub mod manager;
 pub mod source;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct FileId<'ctx>(pub Arc<str, &'ctx Arena>);
-
-#[cfg(feature = "serde")]
-impl<'ctx> serde::Serialize for FileId<'ctx> {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-  where
-    S: serde::Serializer,
-  {
-    serializer.serialize_str(self.0.as_ref())
-  }
-}
-impl<'ctx> AsRef<str> for FileId<'ctx> {
-  fn as_ref(&self) -> &str {
-    &self.0
-  }
-}
-impl<'ctx> Display for FileId<'ctx> {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{}", self.0)
-  }
-}
-
+pub type FileId<'ctx> = &'ctx str;
 pub type Span<'ctx> = (FileId<'ctx>, Range<usize>);
