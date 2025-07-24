e5473ea2981a0755437d3b635590c4d51803a502
Fabrice <fabrice@schaub-dev.xyz>
2025-07-24 14:10:03 +0200

    adds default impl

--- crates/sable-lowering/src/ast.rs
+++ crates/sable-lowering/src/ast.rs
@@ -1,4 +1,21 @@
-use sable_ast::ast::Ast;
+use sable_ast::{
+  ast::Ast,
+  expression::{
+    AssignExpression,
+    BinaryExpression,
+    BlockExpression,
+    Expression,
+    IdentifierExpression,
+    LiteralExpression,
+    VisitExpression,
+  },
+  located::Located,
+  statement::{
+    VariableStatement,
+    VisitStatement,
+  },
+};
+use sable_common::location::Location;
 use sable_hir::package::Package;
 
 pub struct AstLowering<'ast, 'lower, 'hir> {
@@ -13,5 +30,64 @@ impl<'ast, 'lower, 'hir> AstLowering<'ast, 'lower, 'hir> {
 
   pub fn lower(&self) -> Result<(), ()> {
     Ok(())
+  }
+}
+
+impl<'ast, 'lower, 'hir> VisitExpression<'ast> for AstLowering<'ast, 'lower, 'hir> {
+  type Result = ();
+
+  fn visit_block(
+    &mut self,
+    block: &BlockExpression<'ast>,
+    location: &Location<'ast>,
+  ) -> Self::Result {
+    todo!()
+  }
+
+  fn visit_literal(
+    &mut self,
+    literal: &LiteralExpression,
+    location: &Location<'ast>,
+  ) -> Self::Result {
+    todo!()
+  }
+
+  fn visit_assign(
+    &mut self,
+    assign: &AssignExpression<'ast>,
+    location: &Location<'ast>,
+  ) -> Self::Result {
+    todo!()
+  }
+
+  fn visit_binary(
+    &mut self,
+    binary: &BinaryExpression<'ast>,
+    location: &Location<'ast>,
+  ) -> Self::Result {
+    todo!()
+  }
+
+  fn visit_identifier(
+    &mut self,
+    identifier: &IdentifierExpression<'ast>,
+    location: &Location<'ast>,
+  ) -> Self::Result {
+    todo!()
+  }
+}
+
+impl<'ast, 'lower, 'hir> VisitStatement<'ast> for AstLowering<'ast, 'lower, 'hir> {
+  type Result = ();
+
+  fn visit_expression(&mut self, expression: &Expression<'ast>) -> Self::Result {
+    todo!()
+  }
+
+  fn visit_variable_statement(
+    &mut self,
+    variable_statement: &Located<'ast, VariableStatement<'ast>>,
+  ) -> Self::Result {
+    todo!()
   }
 }
